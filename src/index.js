05df4cbf4468af3953871d26a07e6ad0df92e61d
Anna Jachym <ania.jachym@gmail.com>
2019-10-16 05:55:57 +0200

    Segregation of files, adding uuid and classnames.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+import "./styles/main.scss";
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
