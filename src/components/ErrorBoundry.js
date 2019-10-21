2873646b651219a79ea88c4c3f47c670c3022f91
Anna Jachym <ania.jachym@gmail.com>
2019-10-21 11:40:10 +0200

    Create ErrorBoundry component

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class ErrorBoundry extends React.Component {
+    state = {
+        hasError: false
+    }
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        // You can also log the error to an error reporting service
+        console.log("Wystąpił następujący błąd:", error, errorInfo);
+    }
+    render() {
+        const { message, children } = this.props;
+        return this.state.hasError ? message : children;
+    }
+}
+
+ErrorBoundry.propTypes = {
+    message: PropTypes.string.isRequired,
+    children: PropTypes.any.isRequired
+}
+
+export default ErrorBoundry;
