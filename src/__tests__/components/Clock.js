3832bb8ba5a49428d73b988023a7ec94711d93f7
Anna Jachym <ania.jachym@gmail.com>
2019-10-21 14:07:37 +0200

    Test Clock component with ReactTestRenderer and Snapshots

--- src/__tests__/components/Clock.js
+++ src/__tests__/components/Clock.js
@@ -1,49 +1,81 @@
 import React from "react";
 import Clock from "../../components/Clock";
 import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
 
 
 var root = null;
-
+var clockRenderer = null;
 describe('<Clock />', () => {
-    describe('when given minutes and seconds', () => {
-        beforeEach(() => {
-            root = document.createElement("div");
+            describe('when given minutes and seconds (DOM)', () => {
+                beforeEach(() => {
+                    root = document.createElement("div");
+                    ReactDOM.render(
+                        <Clock minutes={10} seconds={20} />, root
+                    )
+                });
+                it('renders properly', () => {
+                
+            expect(root.childNodes[0].nodeName).toEqual("H2")
+            expect(root.childNodes[0].className).toMatch(/Clock/)
+            expect(root.childNodes[0].textContent).toMatch(/10:20/)
+        });
+        it('renders an h2 element', () => {
+            const root = document.createElement("div");
             ReactDOM.render(
                 <Clock minutes={10} seconds={20} />, root
             )
+            expect(root.childNodes[0].nodeName).toEqual("H2")
+
         });
-        it('renders properly', () => {
-        
-     expect(root.childNodes[0].nodeName).toEqual("H2")
-    expect(root.childNodes[0].className).toMatch(/Clock/)
-    expect(root.childNodes[0].textContent).toMatch(/10:20/)
-});
-it('renders an h2 element', () => {
-    const root = document.createElement("div");
-    ReactDOM.render(
-        <Clock minutes={10} seconds={20} />, root
-    )
-    expect(root.childNodes[0].nodeName).toEqual("H2")
+        it('sets a Clock className', () => {
+            const root = document.createElement("div");
+            ReactDOM.render(
+                <Clock minutes={10} seconds={20} />, root
+            )
 
-});
-it('sets a Clock className', () => {
-    const root = document.createElement("div");
-    ReactDOM.render(
-        <Clock minutes={10} seconds={20} />, root
-    )
+            expect(root.childNodes[0].className).toMatch(/Clock/)
 
-    expect(root.childNodes[0].className).toMatch(/Clock/)
+        });
+        it('renders time properly', () => {
+            const root = document.createElement("div");
+            ReactDOM.render(
+                <Clock minutes={10} seconds={20} />, root
+            )
 
-});
-it('renders timeproperly', () => {
-    const root = document.createElement("div");
-    ReactDOM.render(
-        <Clock minutes={10} seconds={20} />, root
-    )
+            expect(root.childNodes[0].textContent).toMatch(/10:20/)
+        });
+    });
 
-    expect(root.childNodes[0].textContent).toMatch(/10:20/)
-});
+            describe('when given minutes and seconds (TestRender)', () => {
+                beforeEach(() => {
+                    clockRenderer = renderer.create(
+                        <Clock minutes={10} seconds={20} />
+                    );
+                });
+                it('renders properly', () => {
+                    expect(clockRenderer.toJSON()).toMatchSnapshot();
+                    
+                });
+                it('renders an h2 element', () => {
+                   
+                    expect(clockRenderer.toJSON().type).toEqual("h2")
+
+                });
+                it('sets a Clock className', () => {
+                    
+
+                    expect(clockRenderer.toJSON().props).toMatchObject({ "className": expect.stringMatching(/Clock/) })
+
+                });
+                it('renders timeproperly', () => {
+                  
+                    
+
+                    expect(clockRenderer.toJSON().children).toEqual(expect.arrayContaining(["10", "20"]))
+                });
+
+
 
 
 
@@ -52,5 +84,5 @@ it('sets className to empty string if not given anything else', () => {
 });
 
     })
-    
-});
+})
+
